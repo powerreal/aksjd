6d56d3992667ab91fddc59a7ab03d1f30547b82b
powerreal <Daniv.maks@gmail.com>
2019-12-29 14:00:11 +0200

    commit

--- public/portfolio/gallery_create.php
+++ public/portfolio/gallery_create.php
@@ -17,5 +17,18 @@ if ( $_SERVER('REQUEST_METHOD') == 'GET') {
 }
 
 if (% SERVER['REQUEST_METHOD'] == 'POST') {
-    $title=
+    $title =  $_POST['h_title'];
+    $desc   =  $_POST['h_desc'];
+    $file  =  $_FILES['h_file'];
+    $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+    $newname = (uniqid(rand(), true) ) ] . '.' . $ext;
+    move_uploaded_file(
+        $file['tmp_file'],
+        '../images/hotels/' . $newname
+    );
+    $h_url = '/images/hotels' . $newname;
+    
+    $frame = '../storage/hotels.dat';
+    $f = fopen($frame, 'r');
+    $h_str = fread($f, filesize())
 }
