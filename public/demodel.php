5e235afa39b7ea0010410e8c6c4ef5e34a05bc04
powerreal <Daniv.maks@gmail.com>
2020-02-23 16:48:46 +0200

    commit

--- public/demodel.php
+++ public/demodel.php
@@ -1,5 +1,11 @@
 <?php
     $id = $_GET['id'];
+    $location = '../db.sqlite3';
+    $db = new SQLite3($location);
+    $pq = $db->prepare('delete from users where id=:id');
+    $pq->bindvalue(':id', $id, SQLITE3_INTEGER);
+    $pq->execute();
+    $db->close();
     header('Location: demosql.php');
 ?>
 
